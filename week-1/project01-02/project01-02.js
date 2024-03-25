cc88ac5ae505992b5c95a8390dbd823700e31fa6
Lisa Gee <lisadgee@hotmail.com>
2024-03-24 20:03:07 -0400

    Week 1 first commit

---
+++ week-1/project01-02/project01-02.js
@@ -0,0 +1,14 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Lisa Gee
+    Date: 3/22/24    
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+var service1Name = "Basic", service2Name = "Express", service3Name = "Extreme",
+service4Name = "Ultimate", service4Name, service1Speed="50 Mbps", service2Speed = "100 Mbps",
+service3Speed = "500 Mbps", service4Speed = "1 Gig";
